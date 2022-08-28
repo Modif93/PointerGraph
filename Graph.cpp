1f116543b85999a2be40d25b381771b25442a0b8
Modif93 <57270858+Modif93@users.noreply.github.com>
2022-08-29 08:14:04 +0900

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,101 +1,242 @@
-#pragma once
 #include <iostream>
 #include <vector>
-#pragma once
+using namespace std;
 
-#define MAX_EDGE 6
-#define MAX_NODES 5242
-#define MAX_EDGES 48880
+#include "Graph.h"
 
-class GraphNode;
-class GraphEdge;
+//////////////////////////
+// GraphEdge Source
+//////////////////////////
+
+GraphEdge::GraphEdge(GraphNode* from, GraphNode* to, int trailNum)
+{
+	this->from = from;
+	this->to = to;
+	this->trailNum = trailNum;
+	this->edgeId = trailNum;
+}
+
+GraphEdge::GraphEdge(int trailNum)
+{
+	from = nullptr;
+	to = nullptr;
+	this->trailNum = trailNum;
+	edgeId = -1;
+}
+
+GraphNode* GraphEdge::getNext(GraphNode* from)
+{
+	if (this->from == from)
+	{
+		return this->to;
+	}
+	else if (this->to == from)
+	{
+		return this->from;
+	}
+	else return nullptr;
+}
 
 
+//////////////////////////
+// GraphNode Source
+//////////////////////////
+
+GraphNode::GraphNode()
+{
+	this->nodeId = -1;
+	for (int i = 0; i < MAX_EDGE; i++)
+	{
+		edges[i] = nullptr;
+	}
+}
+
+GraphNode::GraphNode(int nodeId)
+{
+	this->nodeId = nodeId;
+	for (int i = 0; i < MAX_EDGE; i++)
+	{
+		edges[i] = nullptr;
+	}
+}
+
+GraphEdge* GraphNode::findEdge(int edgeId)
+{
+	for (int i = 0; i < MAX_EDGE;i++) {
+		if (edges[i] == nullptr) return nullptr;
 
 
+		if (edges[i]->edgeId == edgeId) return edges[i];
+	}
+	return nullptr;
+}
 
-typedef struct GraphRoute {
-	int edgeId;
-	int nodeId;
-};
-typedef struct TotalRoute {
-	std::vector<GraphRoute> route;
-};
 
-class GraphNode {
-	// Method Section
-public:
-	GraphNode();
-	GraphNode(int nodeId);
-	int addEdge(GraphEdge* edge);
+int GraphNode::addEdge(GraphEdge* edge)
+{
+	if (this == nullptr) return 1;
+	//already inserted
+	for(int i=0;i< MAX_EDGE;i++)
+	{
+		if (edges[i] != nullptr)
+		{
+			if (edges[i]->edgeId == edge->edgeId)
+				return 1;
+		}
+	}
 	
-	GraphEdge* findEdge(int edgeNum);
 	
+	//reached max
+	if (edgeCnt == MAX_EDGE) return 1;
+	edges[edgeCnt] = edge;
+	edgeCnt++;
+
+	return 0;
+}
+
+
+//////////////////////////
+// Graph Source
+//////////////////////////
+
+void Graph::addNode(GraphNode* node)
+{
+	nodeList.push_back(node);
+	nodeCnt++;
+}
+
+GraphNode* Graph::findNode(int nodeId)
+{
+	for (int i = 0; i < nodeList.size(); i++)
+	{
+		if (nodeList[i]->nodeId == nodeId)
+			return nodeList[i];
+	}
+	return nullptr;
+}
+
+
+
+GraphEdge* Graph::findGraphEdge(int edgeId)
+{
+	for (auto i : edgeList)
+	{
+		if (i->edgeId == edgeId) return i;
+	}
+	return nullptr;
+}
+
+int Graph::addEdge(GraphEdge* edge)
+{
+	if (edge == nullptr)return 1;
+	if (findGraphEdge(edge->edgeId) != nullptr) return 2;
+
+
+	edge->from->addEdge(edge);
+	edge->to->addEdge(edge);
+	edgeCnt++;
+	
+
+	return 0;
+}
+
+bool Graph::isReachable(GraphNode* from, GraphNode* to)
+{
+	checkId = to->nodeId;
+	fromId = from->nodeId;
+	cout << "route from "<< from->nodeId <<" to " << checkId << endl;
 	
 
 	
+	for (int i = 0; i < MAX_EDGE; i++)
+		routes[i] = new TotalRoute;
 
-protected:
-private:
-
-	//Data & Interface Section
-public:
-	//TrailPoint* point;
-	GraphEdge* edges[MAX_EDGE];
-	int nodeId;
-	int edgeCnt = 0;
-protected:
-private:
-};
-
-
-class GraphEdge {
-	// Method Section
-public:
-	GraphEdge(GraphNode* from, GraphNode* to, int trailNum);
-	GraphEdge(int trailNum);
-	GraphNode* getNext(GraphNode* from);
-protected:
-private:
-
-	//Data & Interface Section
-public:
-	GraphNode* from;
-	GraphNode* to;
-	int edgeId;
-	int trailNum;
-
-protected:
-private:
-};
+	//do dfs
+	bool reachable = dfs(from);
+	if (reachable)
+	{
+		cout << "route : n"<< fromId;
+		for (auto& rr : routes[totalRouteIndex]->route) {
+			if (rr.nodeId == fromId) continue;
+			printf("-(%d)->n%d", rr.edgeId, rr.nodeId);
+		}
+		cout << endl;
+	}
+		
 
 
-class Graph {
-	// Method Section
-public:
-	std::vector<GraphEdge*> edgeList;
-	std::vector<GraphNode*> nodeList;
-	int edgeCnt = 0;
-	int nodeCnt = 0;
-	void addNode(GraphNode* node);
-	GraphNode* findNode(int nodeId);
+	reachable = found;
+
+	found = false;
+	visited.clear();
+
+	for (int i = 0; i < MAX_EDGE; i++)
+		delete(routes[i]);
+
+	return reachable;
+}
 
 
-	GraphEdge* findGraphEdge(int edgeId);
+bool Graph::dfs(GraphNode* from) {
+	if (from == nullptr) return false;
+	if (found == true) return found;
+	
+	//printf("n%d", from->nodeId);
 
+	visited.push_back(from->nodeId);
+	
 
-	int addEdge(GraphEdge* edge);
-	bool isReachable(GraphNode* from, GraphNode* to);
-	bool dfs(GraphNode* from);
-protected:
-private:
-	//Data & Interface Section
-public:
-	std::vector<int> visited;
-	TotalRoute* routes[MAX_EDGE];
-	int checkId, fromId, totalRouteIndex;
-	bool found = false;
-protected:
-private:
-};
+	if (checkId == from->nodeId)
+	{
+		found = true;
+		
+		return found;
+	}
 
+	for (int i = 0; i < from->edgeCnt; i++) {
+		if (found == true) break;
+		GraphEdge* edge = from->edges[i];
+		if (edge == nullptr) break;
+		GraphNode* next = edge->getNext(from);
+		if (next == nullptr) continue;
+		
+
+		if (fromId == from->nodeId)
+		{
+			totalRouteIndex = i;
+		}
+
+		bool isVisited = false;
+
+		for (auto& vis : visited) {
+			if (vis == next->nodeId) {
+				isVisited = true; //if visited, skip
+				break;
+			}
+		}
+
+		//recursive
+		if (isVisited == false)
+		{
+			GraphRoute trroute;
+			trroute.nodeId = edge->getNext(from)->nodeId;
+			trroute.edgeId = edge->edgeId;
+			routes[totalRouteIndex]->route.push_back(trroute);
+			//printf("->e%d->", edge->trailNum);
+			if (dfs(edge->getNext(from)) == true)
+			{
+				printf(" found!\n");
+			}
+			else
+			{
+				if (from->nodeId == fromId)
+				{
+					
+				}
+			}
+				
+		}
+		else return false;
+	}
+	
+}
