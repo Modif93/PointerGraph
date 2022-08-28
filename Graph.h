e7e8be2ca7e2ccad02ed5651649cfd444e2b6681
Modif93 <57270858+Modif93@users.noreply.github.com>
2022-08-29 08:13:32 +0900

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,24 +1,49 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#pragma once
+
+#define MAX_EDGE 6
+#define MAX_NODES 5242
+#define MAX_EDGES 48880
 
 class GraphNode;
 class GraphEdge;
 
+
+
+
+
+typedef struct GraphRoute {
+	int edgeId;
+	int nodeId;
+};
+typedef struct TotalRoute {
+	std::vector<GraphRoute> route;
+};
+
 class GraphNode {
 	// Method Section
 public:
-	std::vector<GraphEdge*>edges;
-	int nodeNum;
-	int nodeID;
-	GraphEdge* findEdge(int edgeNum);
-	//TrailPoint* point;
+	GraphNode();
+	GraphNode(int nodeId);
 	int addEdge(GraphEdge* edge);
+	
+	GraphEdge* findEdge(int edgeNum);
+	
+	
+
+	
+
 protected:
 private:
 
 	//Data & Interface Section
 public:
+	//TrailPoint* point;
+	GraphEdge* edges[MAX_EDGE];
+	int nodeId;
+	int edgeCnt = 0;
 protected:
 private:
 };
@@ -37,7 +62,7 @@ private:
 public:
 	GraphNode* from;
 	GraphNode* to;
-	int edgeNum;
+	int edgeId;
 	int trailNum;
 
 protected:
@@ -46,19 +71,31 @@ private:
 
 
 class Graph {
+	// Method Section
 public:
 	std::vector<GraphEdge*> edgeList;
 	std::vector<GraphNode*> nodeList;
 	int edgeCnt = 0;
 	int nodeCnt = 0;
 	void addNode(GraphNode* node);
-	GraphNode* findNode(int nodeNum);
+	GraphNode* findNode(int nodeId);
 
-	GraphEdge* findEdge(GraphEdge* edge);
-	GraphEdge* findEdge(int edgeNum);
+
+	GraphEdge* findGraphEdge(int edgeId);
 
 
 	int addEdge(GraphEdge* edge);
-
+	bool isReachable(GraphNode* from, GraphNode* to);
+	bool dfs(GraphNode* from);
+protected:
+private:
+	//Data & Interface Section
+public:
+	std::vector<int> visited;
+	TotalRoute* routes[MAX_EDGE];
+	int checkId, fromId, totalRouteIndex;
+	bool found = false;
+protected:
+private:
 };
 
